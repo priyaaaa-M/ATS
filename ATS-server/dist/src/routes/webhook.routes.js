4cb4ccf2b7c1297a27bc42fedd6f3bba75372d15
Rishu Kumar <rishukumar9233@gmail.com>
2026-05-03 20:24:31 +0530

    chore: remove dist folder from version control

--- ATS-server/dist/src/routes/webhook.routes.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.webhookRoutes = void 0;
-const express_1 = require("express");
-const webhook_controller_1 = require("../controllers/webhook.controller");
-const router = (0, express_1.Router)();
-exports.webhookRoutes = router;
-router.post('/readai', webhook_controller_1.webhookController.handleReadai);
